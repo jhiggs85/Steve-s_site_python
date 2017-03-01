72125370af97088f88c7d8ff99000725f725a56e
jon <jhiggs8504@gmail.com>
2017-03-01 15:56:00 -0600

    created about page

--- steves_site.py
+++ steves_site.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def homeppage():
     return render_template('homepage.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
