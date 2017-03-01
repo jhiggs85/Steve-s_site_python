234b17d0a9c1e0c0ab0d7d5ceb4ef105c7c10736
jon <jhiggs8504@gmail.com>
2017-03-01 15:52:44 -0600

    created homepage

--- steves_site.py
+++ steves_site.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return render_template('hello_world.html')
+def homeppage():
+    return render_template('homepage.html')
 
 
 if __name__ == '__main__':
